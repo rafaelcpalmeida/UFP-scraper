ce07a7e1704c1ab6c29b9a3d0989c61252e36009
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-02-08 02:28:30 +0000

    Implemented spider to fetch data from teacher alias

---
+++ spider.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from bs4 import BeautifulSoup
+import requests
+import re
+
+#Definir o encoding por omissão como o UTF-8 devido a nomes com acentos
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+if len(sys.argv) > 1:
+    url = 'http://rh.ufp.pt/rh/Detalhe/'+sys.argv[1]
+    r  = requests.get(url)
+    
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
+
+    nome = soup.find("b")
+
+    print nome.contents[0].encode("utf-8")
+    print ""
+    
+    for lists in soup.find_all("ul"):
+        print lists.previous_element
+        for content in lists.contents:
+            print content.contents[0].encode("utf-8")
+        print ""
+    for element in soup.find_all('p'):
+        if re.search(r"Última", str(element)):
+            print element.contents[0]
+            print ""
+        
+        if re.search(r"Correio", str(element)):
+            print element.contents[0]
