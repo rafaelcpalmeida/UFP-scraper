34f765e4856889104e4abf74df13366fd4312b4a
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-03-03 01:17:26 +0000

    Implemented method to update teacher's information when the script is executed

--- updater.py
+++ updater.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import requests
+import sys
+import json
+import os
+
+#Definir o encoding por omissão como o UTF-8 devido a nomes com acentos
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+apiURL = "http://ufp-api.dev/api/v1/"
+
+print ""
+print '+-----------------------------------+'
+print "|  Updating Teachers information... |"
+print '+-----------------------------------+'
+print ""
+
+r  = requests.get(apiURL + "teachers", params={'api_token': 1})
+
+data = json.loads(r.text)
+
+if(data["status"] == "Ok"):
+    for detail in data["message"]:
+        print detail["alias"]
+        os.system("python teachers_updater.py " + detail["alias"])
+else:
+    print "Ocorreu um erro. Mensagem: " + data["message"]
